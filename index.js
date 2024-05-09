df6e42858005048b093625745587e759fbe1919c
Najmulsp <mdnajmulsarker1595@gmail.com>
2024-05-09 23:29:08 +0600

    added token verification

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
@@ -7,9 +9,46 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors())
+app.use(cors({
+  origin:['http://localhost:5173'],
+  credentials: true,
+}))
 app.use(express.json())
+app.use(cookieParser())
 
+// const verifyToken = async(req, res, next) =>{
+//   const token = req.cookies?.token;
+//   console.log('value of token in middleware', token)
+//   if(!token){
+//     return res.status(401).send({message: 'not authorized'})
+//   }
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+//     // err
+//     if(err){
+//       console.log(err)
+//       return res.status(401).send({message: 'unauthorized'})
+//     }
+//     // if token is valid then it would be decoded
+//     console.log('value in the token', decoded)
+//     req.user = decoded;
+//   })
+//   next()
+// }
+
+        // verify jwt middleware
+    const verifyToken = (req, res, next)=>{
+      const token = req.cookies.token;
+            if(token){
+              jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+                if(err){
+                  return err.message;
+                }
+                console.log(decoded)
+                req.user = decoded
+                next()
+              })
+            }
+    }
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.njogpdx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01`
 
@@ -29,8 +68,47 @@ async function run() {
 
     const serviceCollection = client.db('carDoctor').collection('services');
     const orderCollection = client.db('carDoctor').collection('orders');
+        // auth related api
+        // app.post('/jwt', async(req, res) =>{
+        //     const user = req.body;
+        //     console.log(user)
+        //     // const token = jwt.sign(user, 'secret', {expiresIn: '1h'})
+        //     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+        //     res
+        //     .cookie('token', token, {
+        //       httpOnly: true,
+        //       secure: process.env.NODE_ENV === 'production', 
+        //       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        //     })
+        //     .send({success: true})
+        // })
 
+        // cookie token api
+        app.post('/jwt', async(req, res)=>{
+          const user = req.body;
+          const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '365d'});
+
+          res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production'? 'none':'strict',
+          })
+          
+          .send({success: true})
+        })
+        // clear token when logged out
+        app.post('/logout', (req, res)=>{
+          res.clearCookie('token',{
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production'? 'none':'strict',
+            maxAge:0,
+          }).send({success: true})
+        })
+
+        // service related api
         app.get('/services', async(req, res)=>{
+         
             const cursor = serviceCollection.find();
             const result = await cursor.toArray();
             res.send(result)
@@ -44,8 +122,13 @@ async function run() {
         })
 
            // get single service for service details page
-        app.get('/orders', async(req, res) =>{
+        app.get('/orders',verifyToken,  async(req, res) =>{
             console.log(req.query.email)
+            console.log('token asche', req.user)
+            
+            if(req.user.email !== req.query.email){
+              return res.status(403).send({message: ' Access forbidden'})
+            }
             let query = {}
             if(req.query?.email){
                 query={email: req.query.email}
@@ -56,6 +139,7 @@ async function run() {
             // add order by create operation
         app.post('/orders', async(req, res) =>{
             const newOrder =req.body;
+            
             console.log(newOrder)
             const result = await orderCollection.insertOne(newOrder);
             res.send(result)
@@ -70,7 +154,9 @@ async function run() {
         })
 
         // approve orders
-        app.put('/orders/:id', async(req, res)=>{
+        app.put('/orders/:id',verifyToken, async(req, res)=>{
+          const tokenData = req.user;
+          console.log(tokenData, 'from token')
           const id =req.params.id;
           const query = {_id: new ObjectId(id)}
           const updateOrder ={
